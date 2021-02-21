740a30bd8e78922e9f829d02deeac45f774bdc4b
Ayame Ulrich <54376512+AyameU@users.noreply.github.com>
2021-02-20 19:55:18 -0600

    Added CharClass enum

---
+++ TextRPGApp/TextRPG/CharClass.cs
@@ -0,0 +1,28 @@
+﻿namespace TextRPG
+{
+    /// <summary>
+    /// Represents the character class.
+    /// </summary>
+    public enum CharClass
+    {
+        /// <summary>
+        /// Represents the warrior class.
+        /// </summary>
+        Warrior,
+
+        /// <summary>
+        /// Represents the mage class.
+        /// </summary>
+        Mage,
+
+        /// <summary>
+        /// Represents the rogue class.
+        /// </summary>
+        Rogue,
+
+        /// <summary>
+        /// Represents the cleric class.
+        /// </summary>
+        Cleric
+    }
+}
